f2642132c6145d3e6afc67ca2d2c978476e4be0a
DeborahK <deborahk@insteptech.com>
2016-02-29 14:32:52 -0800

    Brought Take 4 up to Angular Beta 7.

--- MovieHunter Take 4 - Routing/app/movies/movie-detail.component.js
+++ MovieHunter Take 4 - Routing/app/movies/movie-detail.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/router', './movie.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './movie.service'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -50,7 +52,7 @@ System.register(['angular2/core', 'angular2/router', './movie.service'], functio
                     __metadata('design:paramtypes', [movie_service_1.MovieService, router_1.Router, router_1.RouteParams])
                 ], MovieDetailComponent);
                 return MovieDetailComponent;
-            })();
+            }());
             exports_1("MovieDetailComponent", MovieDetailComponent);
         }
     }
--- MovieHunter Take 4 - Routing/app/movies/movie-list.component.js
+++ MovieHunter Take 4 - Routing/app/movies/movie-list.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/router', './movie.service', './movieFilter.pipe'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './movie.service', './movieFilter.pipe'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -52,7 +54,7 @@ System.register(['angular2/core', 'angular2/router', './movie.service', './movie
                     __metadata('design:paramtypes', [movie_service_1.MovieService])
                 ], MovieListComponent);
                 return MovieListComponent;
-            })();
+            }());
             exports_1("MovieListComponent", MovieListComponent);
         }
     }
