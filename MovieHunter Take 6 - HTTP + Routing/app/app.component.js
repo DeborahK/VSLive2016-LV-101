50e396923c0ffc5e672b464f1c8823775529055d
DeborahK <deborahk@insteptech.com>
2016-02-29 14:53:53 -0800

    Brought Take 6 Update to Angular 2 Beta 7

--- MovieHunter Take 6 - HTTP + Routing/app/app.component.js
+++ MovieHunter Take 6 - HTTP + Routing/app/app.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/http', 'angular2/router', './home/welcome.component', './movies/movie-list.component', './movies/movie-detail.component', './movies/movie.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/http', 'angular2/router', './home/welcome.component', './movies/movie-list.component', './movies/movie-detail.component', './movies/movie.service'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -56,7 +58,7 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', './home/we
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
--- MovieHunter Take 6 - HTTP + Routing/app/movies/movie-detail.component.js
+++ MovieHunter Take 6 - HTTP + Routing/app/movies/movie-detail.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/router', './movie.service', "../shared/star.component"], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './movie.service', "../shared/star.component"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -52,7 +54,7 @@ System.register(['angular2/core', 'angular2/router', './movie.service', "../shar
                     __metadata('design:paramtypes', [movie_service_1.MovieService, router_1.RouteParams])
                 ], MovieDetailComponent);
                 return MovieDetailComponent;
-            })();
+            }());
             exports_1("MovieDetailComponent", MovieDetailComponent);
         }
     }
